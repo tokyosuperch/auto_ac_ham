37f74198c4bcfd01c2ec55e6a0e92f7b8cc6522c
tokyosuperch <supachan@sfc.wide.ad.jp>
2021-05-22 14:03:59 +0900

    hammer

--- auto_ac_ham.js
+++ auto_ac_ham.js
@@ -35,55 +35,107 @@ var oArgs = WScript.Arguments;
 var arg = oArgs.Unnamed(0);
 // WScript.Echo();
 var str = file_open(arg, "Shift-JIS");
-var line = str.split('\r\n');
+var linearr = str.split('\r\n');
+
+var accent_length = 60;
+var hammering_length = 240;
+var division = 480;
+var time_sig = 4;
 
 var position = {};
 
-for (var l = 0; l < line.length; l++) {
-    if (line[l].split("|").length < 4) {
-        WScript.Echo(line[l]);
+for (var lnum = 0; lnum < linearr.length; lnum++) {
+    if (linearr[lnum].split("|").length < 4) {
+        WScript.Echo(linearr[lnum]);
         continue;
     }
-    if (line[l].match("MThd")) {
-        WScript.Echo(file_init(line, l));
+    if (linearr[lnum].match("MThd")) {
+        WScript.Echo(file_init(linearr, lnum));
         continue;
     }
-    if (line[l].match("On Note")) {
-        WScript.Echo(on_note(line, l));
+    if (linearr[lnum].match("Off Note")) {
+        WScript.Echo(off_note(linearr, lnum));
         continue;
 	}
+    WScript.Echo(linearr[lnum]);
 };
 
-function file_init(line, l) {
-    var pipe = line[l].split("|");
+function file_init(linearr, lnum) {
+    var pipe = linearr[lnum].split("|");
     return(pipe.join("|"));
 };
 
-function on_note(line, l) {
-    return note_time(line, l, null, null, null);
-    Console.WriteLine("\t" + String.Format("{0,3}", currenttick + 60) +" |Pitch Wheel | chan= 1   | bend=4096");
-    Console.WriteLine("\t" + String.Format("{0,3}", currenttick + 120) + " |Pitch Wheel | chan= 1   | bend=0");
+function off_note(linearr, off_num) {
+    var res_text = "";
+    var timestr_off = event_time(linearr, off_num, null, null, null);
+    var timestr_on;
+    var on_num;
+    for (on_num = off_num; on_num >= 0; on_num--) {
+        if (linearr[on_num].match("On Note")) {
+            timestr_on = event_time(linearr, on_num, null, null, null);
+            break;
+		}
+	};
+    if (note_length(timestr_on, timestr_off) >= hammering_length * 4) {
+        res_text += event_time(linearr, on_num, Number(timestr_on.split(":")[0]), Number(timestr_on.split(":")[1]), Number(timestr_on.split(":")[2])) + "|Pitch Wheel | chan= 1   | bend=-1396\r\n";
+        res_text += event_time_add(linearr, on_num, Number(timestr_on.split(":")[0]), Number(timestr_on.split(":")[1]), Number(timestr_on.split(":")[2]), hammering_length) + "|Pitch Wheel | chan= 1   | bend=0\r\n";
+    }
+    res_text += linearr[off_num];
+    return res_text;
+    // Console.WriteLine("\t" + String.Format("{0,3}", currenttick + 60) +" |Pitch Wheel | chan= 1   | bend=4096");
+    // Console.WriteLine("\t" + String.Format("{0,3}", currenttick + 120) + " |Pitch Wheel | chan= 1   | bend=0");
 };
 
-function note_time(line, l, mea, beat, tick) {
-    var pipe = line[l].split("|");
+function event_time_add(linearr, lnum, mea, beat, tick, length) {
+    var new_tick = tick + length;
+    var new_beat = beat;
+    var new_mea = mea;
+    if (new_tick >= division) {
+        new_tick -= division;
+        new_beat += 1;
+	}
+    if (new_beat > time_sig) {
+        new_beat -= 4;
+        new_mea += 1;
+	}
+    return event_time(linearr, lnum, new_mea, new_beat, new_tick);
+};
+
+function event_time(linearr, lnum, mea, beat, tick) {
+    var pipe = linearr[lnum].split("|");
     var timearr = pipe[0].split(":");
     
     if (timearr.length == 1) {
-        if (timearr[0].match(/0-9/)) {
+        if (timearr[0].match(/[0-9]/)) {
             if (tick == null) tick = Number(timearr[0]);
 		}
-        return note_time(line, l-1, mea, beat, tick);
+        return event_time(linearr, lnum-1, mea, beat, tick);
 	} else if (timearr.length == 2) {
         if (beat == null) beat = Number(timearr[0]);
         if (tick == null) tick = Number(timearr[1]);
-        return note_time(line, l-1, mea, beat, tick);
+        return event_time(linearr, lnum-1, mea, beat, tick);
 	} else if (timearr.length == 3) {
-        mea = Number(timearr[0]);
+        if (mea == null) mea = Number(timearr[0]);
         if (beat == null) beat = Number(timearr[1]);
         if (tick == null) tick = Number(timearr[2]);
         return ("    " + mea).substr(-4) + ":" + ("  " + beat ).substr(-2) + ":" + ("   " + tick ).substr(-3) + " ";
 	} else {
         return null;
 	}
+};
+
+function note_length(timestr_on, timestr_off) {
+    var off_arr = timestr_off.split(":");
+    var on_arr = timestr_on.split(":");
+    for (var n = 0; n < off_arr.length; n++) {
+        off_arr[n] = Number(off_arr[n]);
+	};
+    for (var n = 0; n < on_arr.length; n++) {
+        on_arr[n] = Number(on_arr[n]);
+	};
+
+    var totaltick = {};
+    totaltick.off = ((off_arr[0] - 1) * time_sig * division) + ((off_arr[1] - 1) * division) + off_arr[2];
+    totaltick.on = ((on_arr[0] - 1) * time_sig * division) + ((on_arr[1] - 1) * division) + on_arr[2];
+    return totaltick.off - totaltick.on;
 };
